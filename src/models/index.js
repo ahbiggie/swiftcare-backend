d62fe79575a20feffbbd30f03f83a018d91c0ef7
shaibu lawal yusuf <ahbiggielawal@yahoo.com>
2026-07-24 20:41:00 +0100

    feat(queue): add QueueEntry model and migration

--- src/models/index.js
+++ src/models/index.js
@@ -2,12 +2,14 @@ import sequelize from '../config/database.js';
 import definePatient from './patient.js';
 import defineClinic from './clinic.js';
 import defineStaff from './staff.js';
+import defineQueueEntry from './queueEntry.js';
 
 const Patient = definePatient(sequelize);
 const Clinic = defineClinic(sequelize);
 const Staff = defineStaff(sequelize);
+const QueueEntry = defineQueueEntry(sequelize);
 
-const db = { Patient, Clinic, Staff };
+const db = { Patient, Clinic, Staff, QueueEntry };
 
 Object.values(db).forEach((model) => {
   if (typeof model.associate === 'function') model.associate(db);
