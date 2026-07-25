a5b80d15cd288b9d85a10d7ead75b392f96eed71
Victor Olusegun <teeveeolusegun@gmail.com>
2026-07-25 03:18:54 +0100

    modified src/models/index.js, defined appointment in the file

--- src/models/index.js
+++ src/models/index.js
@@ -2,12 +2,14 @@ import sequelize from '../config/database.js';
 import definePatient from './patient.js';
 import defineClinic from './clinic.js';
 import defineStaff from './staff.js';
+import defineAppointment from './appointment.js';
 
 const Patient = definePatient(sequelize);
 const Clinic = defineClinic(sequelize);
 const Staff = defineStaff(sequelize);
+const Appointment = defineAppointment(sequelize);
 
-const db = { Patient, Clinic, Staff };
+const db = { Patient, Clinic, Staff, Appointment };
 
 Object.values(db).forEach((model) => {
   if (typeof model.associate === 'function') model.associate(db);
